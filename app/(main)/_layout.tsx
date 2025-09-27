1bc3fd0f631f4530593070cd1b19be6e34344716
mahmoud salah <mahmoud.s.m619@gmail.com>
2025-09-28 02:18:48 +0300

    feat: refactor layout by removing unused screens and updating navigation structure

--- app/(main)/_layout.tsx
+++ app/(main)/_layout.tsx
@@ -4,31 +4,18 @@ import { Stack } from 'expo-router';
 export default function _layout() {
   return (
     <Stack screenOptions={MainScreenOptions}>
-      <Stack.Screen name="screen1/index" initialParams={{ title: 'Screen 1 Header' }} />
-      <Stack.Screen
-        name="screen2/index"
-        initialParams={{
-          title: 'Screen 2',
-        }}
-      />
-      <Stack.Screen
-        name="screen3/index"
-        initialParams={{
-          title: 'Screen 3',
-        }}
-      />
       <Stack.Screen name="(tabs)" initialParams={{ hasLogo: true }} />
-      <Stack.Screen 
-        name="maintenance-details" 
-        options={{ 
+      <Stack.Screen
+        name="maintenance-details"
+        options={{
           headerShown: false,
-        }} 
+        }}
       />
-      <Stack.Screen 
-        name="services" 
-        options={{ 
+      <Stack.Screen
+        name="services"
+        options={{
           headerShown: false,
-        }} 
+        }}
       />
     </Stack>
   );
