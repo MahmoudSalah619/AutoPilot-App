5041a8c7de727d938d36f4e7152aff621f70a208
mahmoud salah <mahmoud.s.m619@gmail.com>
2025-09-28 02:18:02 +0300

    feat: Update Domain URL and enhance Maintenance component
    - Changed Domain URL to 'http://192.168.1.7:3002' for development.
    - Refactored Maintenance component to improve structure and readability.
    - Added utility functions for handling maintenance data.
    - Implemented filter modal for better maintenance record management.
    - Enhanced styling for various components including maintenance cards and filter modal.
    - Updated ControllableInput to provide better error handling for required fields.
    - Adjusted AppTabBar height for improved layout.

--- apis/Domain.ts
+++ apis/Domain.ts
@@ -1,7 +1,7 @@
 // Backend URLs for different environments
 
 // Use development URL in dev mode
-const DomainUrl = 'http://10.0.2.2:3003';
-// const DomainUrl = 'http://192.168.1.15:3003';
+// const DomainUrl = 'http://10.0.2.2:3003';
+const DomainUrl = 'http://192.168.1.7:3002'; // Dodda
 
 export default DomainUrl;
