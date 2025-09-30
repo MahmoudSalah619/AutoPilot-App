914e9c2ccba86e82e8e9bd43758c883c7fde20e3
mahmoud salah <mahmoud.s.m619@gmail.com>
2025-09-30 03:38:43 +0300

    feat: refactor reminder API queries to use dynamic vehicle ID retrieval

--- apis/services/services/reminders.ts
+++ apis/services/services/reminders.ts
@@ -1,38 +1,51 @@
 import api from '@/apis';
 import { ReminderRequest, ReminderResponse, ReminderItem } from '@/apis/@types/reminder';
+import { getVehicleId } from '@/utils/getVehicleId';
 
 export const reminderApi = api.injectEndpoints({
   endpoints: (build) => ({
     getReminders: build.query<ReminderResponse, void>({
-      query: () => ({
-        url: `/vehicles/${147}/reminders/`,
-      }),
+      query: () => {
+        const vehicleId = getVehicleId();
+        return {
+          url: `/vehicles/${vehicleId}/reminders/`,
+        };
+      },
       providesTags: ['reminder'],
     }),
 
     addReminder: build.mutation<ReminderItem, ReminderRequest>({
-      query: (body) => ({
-        url: `/vehicles/${147}/reminders/`,
-        method: 'POST',
-        body: body,
-      }),
+      query: (body) => {
+        const vehicleId = getVehicleId();
+        return {
+          url: `/vehicles/${vehicleId}/reminders/`,
+          method: 'POST',
+          body: body,
+        };
+      },
       invalidatesTags: ['reminder'],
     }),
 
     updateReminder: build.mutation<ReminderItem, ReminderRequest & { reminderId: string }>({
-      query: ({ reminderId, ...body }) => ({
-        url: `/vehicles/${147}/reminders/${reminderId}/`,
-        method: 'PUT',
-        body: body,
-      }),
+      query: ({ reminderId, ...body }) => {
+        const vehicleId = getVehicleId();
+        return {
+          url: `/vehicles/${vehicleId}/reminders/${reminderId}/`,
+          method: 'PUT',
+          body: body,
+        };
+      },
       invalidatesTags: ['reminder'],
     }),
 
     deleteReminder: build.mutation<void, { reminderId: string }>({
-      query: ({ reminderId }) => ({
-        url: `/vehicles/${147}/reminders/${reminderId}/`,
-        method: 'DELETE',
-      }),
+      query: ({ reminderId }) => {
+        const vehicleId = getVehicleId();
+        return {
+          url: `/vehicles/${vehicleId}/reminders/${reminderId}/`,
+          method: 'DELETE',
+        };
+      },
       invalidatesTags: ['reminder'],
     }),
   }),
